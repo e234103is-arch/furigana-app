90efd487260b9dc3830292f5d013f695546b30cf
e234103is-arch <e234103is@kurume-it.ac.jp>
2026-01-19 21:35:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const button = document.getElementById("start");
 button.onclick = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: { exact: "environment" }
-      }
+      video: { facingMode: "environment" }
     });
     video.srcObject = stream;
   } catch (e) {
-    alert("外カメラを起動できません");
+    alert("カメラを起動できません");
     console.error(e);
   }
 };
+
 
